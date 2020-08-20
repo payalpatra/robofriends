b0bd3e0ff2866f5fb971054d5dcd11d3badb80b0
payalpatra <payalpatra105@gmail.com>
2020-08-21 00:39:47 +0530

    Updates

---
+++ precache-manifest.0dd6f089db4da337754edd3a8265ff7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ceca3e87e849952f23677605a2c2bf",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e3dc6e3e266f057dacc4",
+    "url": "/robofriends/static/css/main.9e077b73.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3dc6e3e266f057dacc4",
+    "url": "/robofriends/static/js/main.d730e4a9.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
